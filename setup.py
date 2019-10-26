1384d1cd2fe7ba6dbd7bcbb2980e8e3449f569d2
Martijn Braam <martijn@brixit.nl>
2019-10-26 15:41:06 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='ofonoctl',
+    version='0.1.0',
+    packages=['ofonoctl'],
+    url='https://git.sr.ht/~martijnbraam/ofonoctl',
+    license='MIT',
+    author='Martijn Braam',
+    author_email='martijn@brixit.nl',
+    description='test/control application for the ofono deamon',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ofonoctl=ofonoctl.__init__:main'
+        ]
+    }
+)
